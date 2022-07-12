e8093e377f6128169031f9d13d182690d8573121
jlvihv <vihv@qq.com>
2022-07-12 17:52:13 +0800

    first commit

---
+++ actuator_test.go
@@ -0,0 +1,27 @@
+package docker_compose_actuator
+
+import (
+	"context"
+	"testing"
+)
+
+func TestCompose(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		args []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"test", args{context.Background(), []string{"up", "echo"}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Compose(tt.args.ctx, tt.args.args); (err != nil) != tt.wantErr {
+				t.Errorf("Compose() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
